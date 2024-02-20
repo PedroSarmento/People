93f1d7dcc7cfef0178eeab33eae293a86651e16a
Pedro Sarmento <pedro.hpsarmento@gmail.com>
2024-02-19 22:06:11 -0300

    Altera forma de inicializar o mysql para não ser necessário commitar os arquivos de banco

--- node/index.js
+++ node/index.js
@@ -2,25 +2,38 @@ const express = require('express')
 const mysql = require('mysql')
 const app = express()
 const port = 3000
-const mysql_config = {
+const db = mysql.createConnection({
     host: "db",
     user: "root",
     password:"root",
     database: "nodedb"
-};
+});
+
+db.connect((err) => {
+    if (err) throw err;
+    console.log('Connected to MySQL');
+});
 
 app.get('/', (req,res) => {
-    const select_query = `SELECT * FROM people;`
-
-    var names = '';
-
-    const connection = mysql.createConnection(mysql_config)
-    connection.query(select_query, function (err, result, fields) {
-        result.map(value => names += "<p>" + value.name + "</p>")
-        res.send("<h1>Full Cycle Rocks!</h1><br>" + names)
+    const name = getName();
+    const sql = `INSERT INTO people (name) VALUES ('${name}')`;
+    db.query(sql, (err) => {
+        if (err) throw err;
+        console.log(`Inserted name: ${name}`);
     });
-    connection.end()
+    db.query('SELECT name FROM people', (err, results) => {
+        if (err) throw err;
+        const names = results.map(result => result.name).join('<br>');
+        res.send(`<h1>Full Cycle Rocks!</h1><br>- Nomes cadastrados: <br> ${names}`);
+    });
 })
+
+const getName = () => {
+    const names = ["Luan","Pedro","Caio", "Mariana", "Lígia", "Rafaela", "Andrei Coelho", "Fulano Santos", "Ciclano Pedra"];
+    const sobrenome =  ["Da silva","Dos santos","Pereira","Alves","Ferreira","De oliveira","Silva","Rodrigues","De souza","Gomes","Santos","Oliveira","Ribeiro","De jesus","Martins","Soares","Barbosa","Lopes","Vieira","Souza","Fernandes","Lima","Costa","Batista","De sousa","Dias","De lima","Do nascimento","Moreira","Nunes","Da costa","De almeida"];
+
+    return names[Math.floor(Math.random() * names.length)] + " " + sobrenome[Math.floor(Math.random() * sobrenome.length)];
+};
 
 app.listen(port, () => {
     console.log("Rodando na porta " + port)
